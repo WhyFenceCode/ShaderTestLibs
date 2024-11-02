402657f1085766410838d7cb7d416d59030cac0f
WhyFenceCode <120431739+WhyFenceCode@users.noreply.github.com>
2024-11-02 08:48:03 -0700

    Update scattering.glsl

--- scattering.glsl
+++ scattering.glsl
@@ -2,7 +2,7 @@ float cos2(float theta){
   return (cos(2.0 * theta)+  1.0) / 2.0;
 }
 
-float henyey-greenstein(float g, float angle) {
+float henyeyGreenstein(float g, float angle) {
   float front = (3.0 * (1.0 - pow(g, 2))) / (2.0 * (2.0 + pow(g, 2.0);
   float back = (1.0 + cos2(angle)) / pow((1.0 + pow(g, 2.0) - 2.0 * g * cos(angle)), 3.0 / 2.0);
   return front * back;
