b4e2ab9bd1f61ee59a073f786d8f665b2a83a502
WhyFenceCode <120431739+WhyFenceCode@users.noreply.github.com>
2024-11-02 21:57:19 -0700

    Update scattering.glsl

--- scattering.glsl
+++ scattering.glsl
@@ -1,3 +1,8 @@
+#define MAX_STEPS 10
+#define ALLOWED_DEVIATION 0.01
+#earthSize 6400.0
+#atmoThickness 100.0
+
 float cos2(float theta){
   return (cos(2.0 * theta)+  1.0) / 2.0;
 }
@@ -8,6 +13,25 @@ float lengthVec(vec3 p1, vec3 p2){
 
 float distanceToCircle(vec3 p, vec3 center, float r){
   return lengthVec(p, center) - r;
+}
+
+//pos is vec3(0.0, playerY, 0.0);
+//dir is ray direction normalised
+float sphereMarch(vec3 pos, vec3 dir){
+  float progress = 0.0;
+  vec3 samplePoint = pos;
+
+  for(int i=0; i < MAX_STEPS; ++i)
+  {
+    samplePoint = pos + dir * progress;
+    float distance = distanceToCircle(samplePoint, vec3(0.0, -earthSize, 0.0), atmoThickness);
+    progress += distance;
+    if(distance < ALLOWED_DEVIATION){
+      return progress;
+    }
+  }
+
+  return 0.0;
 }
 
 float henyeyGreenstein(float g, float angle) {
